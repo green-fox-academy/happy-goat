233ed69943b2a2d236a770655f7ca5e0e0c4fa5a
janosdobi <j.dobi88@gmail.com>
2017-11-28 15:16:43 +0100

    A5

--- Greet.java
+++ Greet.java
@@ -1,6 +1,6 @@
 public class Greet {
     
-    public static void greet() {
-        System.out.println("Greetings!");
+    public static void greet(String s1, String s2) {
+        System.out.println(s1, s2);
     }
 }
