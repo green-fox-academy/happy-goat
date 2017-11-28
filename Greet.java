b9885b2c7939a671074d6e7ad0595142dab3d654
Márton Csoma <csoma.m@gmail.com>
2017-11-28 15:30:59 +0100

    b6

--- Greet.java
+++ Greet.java
@@ -1,6 +1,6 @@
 public class Greet {
     
     public static void greet(String s1, String s2) {
-        System.out.println(s1, s2);
+        System.out.println("Greetings for the " + s1 + s2);
     }
 }
